7c080c3018f1fca192d33016d1451e50e9ad6d54
alihi heroku <seyedali.aghayan@gmail.com>
2018-11-02 22:13:12 +0330

    Groups is done

--- Groups/models.py
+++ Groups/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField()
+    description = models.TextField()
+    icon = models.ImageField(upload_to='images/')
+    status = models.BooleanField(default=False)
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime(' %b %e %y ')
+
+    def __str__(self):
+        return self.name
+
--- Groups/views.py
+++ Groups/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
+from .models import Group
+from django.utils import timezone
 def home(request):
     return render(request,'Groups/home.html')
+
+@login_required
+def Create(request):
+	if request.method == 'POST':
+		if request.POST['title'] and request.POST['description'] and request.FILES['icon']:
+			group = Group()
+			group.name = request.POST['title']
+			group.description = request.POST['description']
+			group.icon = request.FILES['icon']
+			group.pub_date = timezone.datetime.now()
+			group.created_by = request.user
+			group.save()
+			return redirect('home')
+	else:
+		messages.success(request,('All fields are required.'))
+		return render(request, 'Groups/create.html')
+
+    # return render(request,'Groups/create.html')
